f402d501bc99710aaea65dfe028c47e24cbcccba
GASC1211 <48170476+Gasc1211@users.noreply.github.com>
2023-07-26 17:57:05 -0600

    Changed datatypes to OracleDB compatible ones

--- db_schema.sql
+++ db_schema.sql
@@ -1,29 +1,29 @@
 USE [Rentadora];
 
 CREATE TABLE Localidades (
-    LocalidadID nchar(5),
-    Direccion nvarchar(100),
-    Telefono nchar(8)
+    LocalidadID char(5),
+    Direccion varchar(100),
+    Telefono char(8)
     CONSTRAINT LocalidadPK PRIMARY KEY (LocalidadID)
 );
 
 CREATE TABLE Clientes (
-    ClienteID nchar(5),
-    Nombres nvarchar(30),
-    Apellidos nvarchar(30),
-    DNI nchar(13),
-    RTN nchar(13),
-    Licencia nchar(13),
-    Celular nchar(8),
+    ClienteID char(5),
+    Nombres varchar(30),
+    Apellidos varchar(30),
+    DNI char(13),
+    RTN char(13),
+    Licencia char(13),
+    Celular char(8),
     CONSTRAINT ClientePK PRIMARY KEY (ClienteID)
 );
 
 CREATE TABLE DatosPago (
-    DatosPagoID nchar(5),
-    ClienteID nchar(5),
-    Direccion nvarchar(100),
-    Num_Tarjeta nchar(16) UNIQUE,
-    CSC nchar(5),
+    DatosPagoID char(5),
+    ClienteID char(5),
+    Direccion varchar(100),
+    Num_Tarjeta char(16) UNIQUE,
+    CSC char(5),
     Efectivo bit,
     CONSTRAINT DatosPagoPK PRIMARY KEY (DatosPagoID),
     CONSTRAINT DatosPagoClienteFK FOREIGN KEY (ClienteID) REFERENCES Clientes(ClienteID)
@@ -31,91 +31,91 @@ CREATE TABLE DatosPago (
 );
 
 CREATE TABLE Puestos (
-    PuestoID nchar(5),
-    Nombre nvarchar(50),
+    PuestoID char(5),
+    Nombre varchar(50),
     CONSTRAINT PuestoPK PRIMARY KEY (PuestoID)
 );
 
 CREATE TABLE Empleado (
-    EmpleadoID nchar(5),
-    Nombres nvarchar(30),
-    Apellidos nvarchar(30),
-    DNI nchar(13) UNIQUE,
-    Celular nchar(8),
-    PuestoID nchar(5),
+    EmpleadoID char(5),
+    Nombres varchar(30),
+    Apellidos varchar(30),
+    DNI char(13) UNIQUE,
+    Celular char(8),
+    PuestoID char(5),
     Sueldo money,
-    Direccion nvarchar(50),
+    Direccion varchar(50),
     CONSTRAINT EmpleadoPK PRIMARY KEY (EmpleadoID),
     CONSTRAINT EmpleadoPuestoFK FOREIGN KEY (PuestoID) REFERENCES Puestos(PuestoID)
 );
 
 CREATE TABLE Marcas (
-    MarcaID nchar(5),
-    Nombre nchar(25),
+    MarcaID char(5),
+    Nombre char(25),
     CONSTRAINT MarcaPK PRIMARY KEY (MarcaID)
 );
 
 CREATE TABLE Categoria (
-    CategoriaID nchar(5),
-    Nombre nchar(25),
+    CategoriaID char(5),
+    Nombre char(25),
     CONSTRAINT CategoriaPK PRIMARY KEY (CategoriaID)
 );
 
 CREATE TABLE Vehiculos (
-    VehiculoID nchar(5),
-    MarcaID nchar(5),
-    CategoriaID nchar(5),
-    ModeloID nchar(5),
-    Color nvarchar(15),
+    VehiculoID char(5),
+    MarcaID char(5),
+    CategoriaID char(5),
+    ModeloID char(5),
+    Color varchar(15),
     Kilometraje float,
-    Combustible nvarchar(10),
+    Combustible varchar(10),
     CONSTRAINT VehiculoPK PRIMARY KEY (VehiculoID),
     CONSTRAINT VehiculoCategoriaFK FOREIGN KEY (CategoriaID) REFERENCES Categoria(CategoriaID),
 	CONSTRAINT VehiculosMarcaFK FOREIGN KEY (MarcaID) REFERENCES Marcas(MarcaID),
 );
 
 CREATE TABLE Daños (
-    DañoID nchar(5),
-    Descripcion nvarchar(100),
+    DañoID char(5),
+    Descripcion varchar(100),
     Costo money,
     CONSTRAINT DañoPK PRIMARY KEY (DañoID)
 );
 
 CREATE TABLE Extras (
-    ExtraID nchar(5),
-    Nombre nvarchar(50),
-    Descripcion nvarchar(100),
+    ExtraID char(5),
+    Nombre varchar(50),
+    Descripcion varchar(100),
     Costo money,
-    CategoriaID nchar(5),
+    CategoriaID char(5),
     CONSTRAINT ExtraPK PRIMARY KEY (ExtraID),
     CONSTRAINT ExtrasCategoriaFK FOREIGN KEY (CategoriaID) REFERENCES Categoria(CategoriaID)
 );
 
 CREATE TABLE Caracteristicas (
-    CaracteristicaID nchar(5),
-    Nombre nvarchar(50),
-    Descripcion nvarchar(100),
+    CaracteristicaID char(5),
+    Nombre varchar(50),
+    Descripcion varchar(100),
     CONSTRAINT CaracteristicaPK PRIMARY KEY (CaracteristicaID)
 );
 
 CREATE TABLE Estatus (
-    EstatusID nchar(5),
-    Nombre nvarchar(50),
-    Descripcion nvarchar(100)
+    EstatusID char(5),
+    Nombre varchar(50),
+    Descripcion varchar(100)
     CONSTRAINT EstatusPK PRIMARY KEY (EstatusID)
 );
 
 CREATE TABLE Ordenes (
-    OrdenID nchar(5),
+    OrdenID char(5),
     Fecha_Hora datetime,
-    EmpleadoID nchar(5),
-    ClienteID nchar(5),
-    VehiculoID nchar(5),
-    Localidad_EntregaID nchar(5),
+    EmpleadoID char(5),
+    ClienteID char(5),
+    VehiculoID char(5),
+    Localidad_EntregaID char(5),
     Fecha_Hora_Entrega datetime,
-    Localidad_DevolucionID nchar(5),
+    Localidad_DevolucionID char(5),
     Fecha_Hora_Devolucion datetime,
-    EstatusID nchar(5),
+    EstatusID char(5),
     CONSTRAINT OrdenPK PRIMARY KEY (OrdenID),
     CONSTRAINT EmpleadoFK FOREIGN KEY (EmpleadoID) REFERENCES Empleado(EmpleadoID),
     CONSTRAINT OrdenesClienteFK FOREIGN KEY (ClienteID) REFERENCES Clientes(ClienteID),
@@ -125,9 +125,9 @@ CREATE TABLE Ordenes (
 );
 
 CREATE TABLE Facturas (
-    OrdenID nchar(5),
-    FacturaID nchar(15),
-    DatosPagoID nchar(5),
+    OrdenID char(5),
+    FacturaID char(15),
+    DatosPagoID char(5),
     Subtotal money,
     Descuento money,
     Impuesto_Venta money,
@@ -139,33 +139,33 @@ CREATE TABLE Facturas (
 );
 
 CREATE TABLE Reporte (
-    ReporteID nchar(5),
-    OrdenID nchar(5),
+    ReporteID char(5),
+    OrdenID char(5),
     Fecha_Hora_Recepcion datetime,
     Kilometraje_Final int,
-    ReceptorID nchar(5),
+    ReceptorID char(5),
     CONSTRAINT ReportePK PRIMARY KEY (ReporteID),
     CONSTRAINT ReporteOrdenFK FOREIGN KEY (OrdenID) REFERENCES Ordenes(OrdenID),
     CONSTRAINT ReporteReceptorFK FOREIGN KEY (ReceptorID) REFERENCES Empleado(EmpleadoID)
 );
 
 CREATE TABLE DañosXReporte (
-    ReporteID nchar(5),
-    DañoID nchar(5),
+    ReporteID char(5),
+    DañoID char(5),
     CONSTRAINT DRReporteFK FOREIGN KEY (ReporteID) REFERENCES Reporte(ReporteID),
     CONSTRAINT DRDañoFK FOREIGN KEY (DañoID) REFERENCES Daños(DañoID)
 );
 
 CREATE TABLE CaracteristicasXVehiculo (
-    VehiculoID nchar(5),
-    CaracteristicaID nchar(5),
+    VehiculoID char(5),
+    CaracteristicaID char(5),
     CONSTRAINT CVVehiculoFK FOREIGN KEY (VehiculoID) REFERENCES Vehiculos(VehiculoID),
     CONSTRAINT CVCaracteristicaFK FOREIGN KEY (CaracteristicaID) REFERENCES Caracteristicas(CaracteristicaID)
 );
 
 CREATE TABLE ExtrasXOrden (
-    OrdenID nchar(5),
-    ExtraID nchar(5),
+    OrdenID char(5),
+    ExtraID char(5),
     CONSTRAINT EOOrdenFK FOREIGN KEY (OrdenID) REFERENCES Ordenes(OrdenID),
     CONSTRAINT EOExtraFK FOREIGN KEY (ExtraID) REFERENCES Extras(ExtraID)
 );
