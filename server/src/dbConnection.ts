1a95bcc6019379f691c28aac6fbfe9876df00b64
GASC1211 <48170476+Gasc1211@users.noreply.github.com>
2023-08-10 17:57:12 -0600

    Modularized system components Co-authored-by: fgutierrezca <fgutierrezca@users.noreply.github.com>

--- server/src/dbConnection.ts
+++ server/src/dbConnection.ts
@@ -1,134 +1,30 @@
-import oracledb from "oracledb";
+import oracledb, { Connection } from "oracledb";
 import * as types from "./types";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const config = require("./config");
 
-const dbConfig = config.default;
-
-async function initConnection() {
+// Attempt to create the connection pool
+async function initConnection(): Promise<Connection | undefined> {
   try {
-    // Attempt a connection to the database
-    await oracledb.createPool(dbConfig);
+    // Get connection properties from config file
+    const { user, password, connectionString } = config.default;
+
+    // Create the connection pool
+    await oracledb.createPool({
+      user: user,
+      password: password,
+      connectionString: connectionString,
+      poolAlias: "MainPool",
+    });
+
+    // Return the connection
+    return await oracledb.getConnection("MainPool");
   } catch (error) {
     console.error(error);
-  } finally {
-    // Close the connection
-    await closeConnectionPool();
   }
 }
 
-async function getUsers() {
-  let connection;
-  try {
-    // Establish a connection to the database server
-    connection = await oracledb.getConnection(dbConfig);
-
-    // SQL statement to execute
-    const sqlStatement: string = `
-    SELECT emp.empID, emp.nombre_emp, pu.desc_puesto
-    FROM Empleado emp 
-    JOIN Puesto pu ON pu.puestoID = emp.puestoID
-    `;
-
-    // Statement execution options
-    const options: Object = {
-      outFormat: oracledb.OUT_FORMAT_OBJECT, // Use OUT_FORMAT_OBJECT for JSON-like results
-    };
-
-    // Execute the SELECT statement
-    const result = await connection.execute(sqlStatement, [], options);
-
-    return result.rows; // Return the retrieved rows
-  } catch (error) {
-    console.log(error);
-    throw error;
-  } finally {
-    // Dispose of the connection
-    if (connection) {
-      try {
-        await connection.close();
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  }
-}
-
-async function logIn(username: String, password: String) {
-  let connection;
-  try {
-    connection = await oracledb.getConnection(dbConfig);
-
-    const sqlStatement: string = `
-    SELECT * FROM Clientes
-    WHERE NOMBREUSUARIO = '${username}'
-    AND CONTRASENIA = '${password}'
-    `;
-
-    const options: Object = {
-      outFormat: oracledb.OUT_FORMAT_OBJECT,
-    };
-
-    const result = await connection.execute(sqlStatement, [], options);
-    return result.rows;
-  } catch (error) {
-    console.log(error);
-    throw error;
-  } finally {
-    if (connection) {
-      try {
-        await connection.close();
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  }
-}
-
-export async function createUser(client: types.Clientes) {
-  let connection;
-  try {
-    // Establish a connection pool to the database sever
-    connection = await oracledb.getConnection(dbConfig);
-
-    // SQL statement to execute
-    const sqlStatement: string = `INSERT INTO Clientes(Nombres, Apellidos, DNI, RTN, Licencia, Celular, CorreoElectronico) VALUES(:1, :2, :3, :4, :5, :6, :7)`;
-
-    // Values for insert statement
-    let params: Array<any> = [
-      client.nombres,
-      client.apellidos,
-      client.dni,
-      client.rtn,
-      client.licencia,
-      client.celular,
-      client.correoElectronico,
-    ];
-
-    // Statement execution options
-    const options: Object = {
-      out: oracledb.OUT_FORMAT_OBJECT,
-    };
-
-    // Execute the statement
-    await connection.execute(sqlStatement, params, options);
-  } catch (error) {
-    console.log(error);
-  } finally {
-    // Dispose of the connection
-    if (connection) {
-      try {
-        await connection.close();
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  }
-}
-
+// Close the connection pool
 async function closeConnectionPool() {
   try {
     await oracledb.getPool().close(10);
@@ -137,6 +33,6 @@ async function closeConnectionPool() {
   }
 }
 
-initConnection();
+const connection: Promise<Connection | undefined> = initConnection();
 
-export { getUsers, logIn };
+export { connection };
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,9 +2,8 @@ import express, { Express, Request, Response } from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 
-import { createUser, getUsers, logIn } from "./dbConnection";
-import * as types from "./types";
 import config from "./config";
+import userModule from "./components/user.module";
 
 const app: Express = express();
 
@@ -13,44 +12,12 @@ app.use(cors()); // Usa el middleware cors para habilitar CORS
 // Configure JSON parser for urlencoded request
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// User Module
+app.use("/users", userModule.router);
+
 // Base URL
 app.get("/", (req: Request, res: Response) => {
   res.send("GearShift Car Rentals Backend");
-});
-
-// Obtener todos los usuarios
-app.get("/getUsers", async (req: Request, res: Response) => {
-  try {
-    const users = await getUsers();
-    res.json(users);
-  } catch (error) {
-    console.error("Error fetching users:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-// Validar en el LogIn que el usuario existe en la base de datos
-app.post("/login", async (req: Request, res: Response) => {
-  const { username, password } = req.body;
-  console.log(username, password);
-  try {
-    const result = await logIn(username, password);
-    res.json(result);
-  } catch (error) {
-    console.error("Error:", error);
-    res.status(500).json({ error: "Ocurrió un error en el servidor." });
-  }
-});
-
-// User Endpoint
-app.post("/user", (req: Request, res: Response) => {
-  const client: types.Clientes = req.body;
-  try {
-    createUser(client);
-    res.status(200).send("Succesfully commited values to the database.");
-  } catch (error) {
-    res.send(500).json({ error: "Failed to commit values to the database." });
-  }
 });
 
 app.listen(config.port, () => {
