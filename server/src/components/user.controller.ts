869d1566cecf8312e79d78fe0763afb403b85d4b
GASC1211 <48170476+Gasc1211@users.noreply.github.com>
2023-08-14 10:05:02 -0600

    Finished user module

--- server/src/components/user.controller.ts
+++ server/src/components/user.controller.ts
@@ -5,32 +5,69 @@ import oracledb, { Connection, Result } from "oracledb";
 
 const userController: any = {
   // Get user data from database
-  async getUsers(req: Request, res: Response) {
-    try {
-      connection.then(async (connection: Connection | undefined) => {
-        // `SELECT * FROM Clientes WHERE ClienteID=${req.body.clientID}`
+  async getUser(req: Request, res: Response) {
+    connection
+      .then(async (connection: Connection | undefined) => {
+        let { clientID } = req.params;
+        let user;
         if (connection !== undefined) {
-          const result = await connection.execute(`SELECT * FROM Clientes`);
-          console.log(result.rows);
+          const result = await connection.execute(
+            `SELECT * FROM Clientes WHERE Clientes.ClienteID = ${clientID}`
+          );
+          user = result.rows;
         } else {
           throw new Error("Failed to connect to the database");
         }
-        let users;
-        res.status(200).send(users);
+        res.status(200).send(user);
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).send("Failed to fetch data from database");
       });
-    } catch (error) {
-      console.log(error);
-      res.status(500).send("Failed to fetch data from database");
-    }
+  },
+  // Get all user data from database
+  async getUsers(req: Request, res: Response) {
+    connection
+      .then(async (connection: Connection | undefined) => {
+        let users;
+        if (connection !== undefined) {
+          const result = await connection.execute(`SELECT * FROM Clientes`);
+          users = result.rows;
+        } else {
+          throw new Error("Failed to connect to the database");
+        }
+        res.status(200).send(users);
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).send("Failed to fetch data from database");
+      });
   },
   // Insert user data into database
   async insertUser(req: Request, res: Response) {
-    try {
-      let client = req.body.client;
-      res.status(200).send();
-    } catch (error) {
-      res.status(500).send("Failed to insert data to the database.");
-    }
+    connection
+      .then(async (connection: Connection | undefined) => {
+        let client = req.body.client;
+        if (connection !== undefined) {
+          const result = await connection.execute(`INSERT INTO 
+            Clientes(Nombres, Apellidos, DNI, RTN, Licencia, Celular, CorreoElectronico) 
+            VALUES (
+              ${client.Nombres},
+              ${client.Apellidos},
+              ${client.DNI},
+              ${client.RTN},
+              ${client.Licencia},
+              ${client.Celular},
+              ${client.CorreoElectronico}
+            )`);
+        } else {
+          throw new Error("Failed to connect to the database");
+        }
+        res.status(200).send();
+      })
+      .catch((error) => {
+        res.status(500).send("Failed to insert data to the database.");
+      });
   },
 };
 
--- server/src/components/user.router.ts
+++ server/src/components/user.router.ts
@@ -4,6 +4,7 @@ import userController from "./user.controller";
 const userRouter = {
   getRouter() {
     const router = express.Router();
+    router.route("/:clientID").get(userController.getUser);
     router.route("/").get(userController.getUsers);
     router.route("/").post(userController.insertUser);
     return router;
